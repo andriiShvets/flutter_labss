54cc10e07c7595322bdd94756f4eae192c1cf81d
Andrii Shvets <62800787+andriiShvets@users.noreply.github.com>
2022-11-21 21:56:31 +0200

    zalik

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:andrii_shvets/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "1b6bbc075cmshae493e2b5ed2dd0p1e2a91jsnfc39e2aa7191",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
